431b9bcf061e1d7abaa370de64188e3694ec96cd
Jared Cline <jareddcline@gmail.com>
2020-06-08 17:21:32 -0400

    changed some comments

--- victrola.py
+++ victrola.py
@@ -17,7 +17,8 @@ async def on_ready():
 
 
 # reads config.ini file and loads in the token for discord and a google sheets document with the music details
-# you'll have to edit config.ini for these
+# you'll have to edit config.ini for these to work properly
+# currently, sheet is unused
 def configure():
     conf = cfp.ConfigParser()
     conf.read("config.ini")
