0f1b9821fa714fcb80d9a567e2a7a55bab5a90b6
Jared Cline <jareddcline@gmail.com>
2020-06-12 00:25:24 -0400

    config now handles a song list as sections -- unfinished

--- victrola.py
+++ victrola.py
@@ -15,6 +15,8 @@ sheet = None
 channel = None
 vc = None
 
+songs = dict()
+
 
 @client.event
 async def on_ready():
@@ -33,8 +35,10 @@ async def on_ready():
     # source = discord.FFmpegPCMAudio('hodgepodge.mp3'), after=lambda e: print('done', e)
     # playing test song
 
-    songName = "song"
 
+# downloads a song in the config.ini list
+# called by configure()
+def downloadSong(songName):
     ydl_opts = {
         'format': 'bestaudio/best',
         'postprocessors': [{
@@ -45,9 +49,12 @@ async def on_ready():
         'outtmpl': songName + '.%(ext)s'
     }
     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-        ydl.download(["https://youtu.be/OlhqQfzaQTk"])
-    vc.play(discord.FFmpegPCMAudio("song.mp3"))
-    # vc.play(source)
+        ydl.download([""])
+
+
+# plays a chosen song
+def playSong(songName):
+    vc.play(discord.FFmpegPCMAudio(songName + ".mp3"))
 
 
 # reads config.ini file and loads in the token for discord and a google sheets document with the music details
@@ -57,11 +64,22 @@ def configure():
     global token
     global guildName
     global sheet
+    global songs
     conf = cfp.ConfigParser()
     conf.read("config.ini")
     token = conf['DEFAULT']['token']
     guildName = conf['DEFAULT']['guild']
     sheet = conf['DEFAULT']['sheet']
+
+    # creating the song list
+    for songStats in conf.sections():
+        print(f"Handling section: {songStats}")
+        # adding song to song dictionary
+        songs[conf[songStats]["songName"]] = conf[songStats]["link"]
+        # if we don't have the song yet, download it
+        if not os.path.exists(conf[songStats]["songName"] + ".mp3"):
+            print(f'downloading song: {conf[songStats]["songName"]}')
+            downloadSong(conf[songStats]["link"])
 
 
 def main():
